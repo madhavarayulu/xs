7a571113be3b7e169aea6a21e58e24e73c239603
madhavarayulu <madhavarayulu@gmail.com>
2024-11-20 14:37:24 +0530

    12th working draft - u4 - 20.Nov.24

--- src/components/pages/connectors/co-s2/CoS2.jsx
+++ src/components/pages/connectors/co-s2/CoS2.jsx
@@ -1,3 +1,4 @@
+// CoS2.jsx
 import React from 'react';
 import './co-s2.css';
 
@@ -10,7 +11,46 @@ const CoS2 = () => {
       <div className="cos2__show-only__mobile">
         <div className="cos2__spacer__88"></div>
       </div>
-      <div className="cos2__contain"></div>
+      <div className="cos2__contain">
+        <div className="cos2__hr__line"></div>
+        <div className="cos2__spacer__60"></div>
+        <div className="cos2__base-grid">
+          <div className="cos2__vertical__32">
+            <h2 className="cos2__text__display-medium">
+              Automate your data movement today
+            </h2>
+            <p className="cos2__text__body-large">
+              Join thousands of companies using Xemsoft to centralize and
+              transform their data.
+            </p>
+            <div className="cos2__button__list">
+              <a href="" className="cos2__button__outline cos2__small">
+                Start free
+              </a>
+              <a href="" className="cos2__button__arrow-1 cos2__blue-60">
+                Contact sales
+              </a>
+              <a href="" className="cos2__button__arrow-2 cos2__blue-60">
+                See plans and pricing
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="cos2__spacer__60"></div>
+        <div className="cos2__hr__line"></div>
+        <div className="cos2__spacer__28"></div>
+        <div className="cos2__button__list">
+          <a href="" className="cos2__button__arrow-1 cos2__blue-60">
+            Custom connectors
+          </a>
+          <a href="" className="cos2__button__arrow-1 cos2__blue-60">
+            Feature Request Portal
+          </a>
+          <a href="" className="cos2__button__arrow-1 cos2__blue-60">
+            Getting started docs
+          </a>
+        </div>
+      </div>
       <div className="cos2__show-only__desktop-mobile">
         <div className="cos2__spacer__160"></div>
       </div>
